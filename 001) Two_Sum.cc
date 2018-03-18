701224505ebb94dfd7f120ff720495d42c79373f
Han Chao Zhao <johnsonjohnson1992@gmail.com>
2018-03-17 20:13:26 -0400

    lala
    lala

--- 001) Two_Sum.cc
+++ 001) Two_Sum.cc
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, int> mapping;
-        vector<int> result;
-        for (int i = 0; i < nums.size(); i++) {
-            mapping[nums[i]] = i;
-        }
-        for (int i = 0; i < nums.size(); i++) {
-            int searched = target - nums[i];
-            if (mapping.find(searched) != mapping.end() && mapping[searched] != i) {
-                result.push_back(i);
-                result.push_back(mapping[searched]);
-                break;
-            }
-        }
-        return result;
-    }
+	vector<int> twoSum(vector<int>& nums, int target) {
+		std::map<int, int> helper;
+		std::map<int, int>::iterator it;
+		vector<int> result;
+		for (int i = 0; i < nums.size(); i++) {
+			it = helper.find(nums[i]);
+			if (it != helper.end()) {
+				if (it->second != i) {
+					result.push_back(i);
+					result.push_back(it->second);
+					return result;
+				}
+			}
+			else {
+				helper[target - nums[i]] = i;
+			}
+		}
+		return result;
+	}
 };
